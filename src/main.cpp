6a966d440775109613d9b4c7447143d709def787
H.Chihoon <to@ihooni.com>
2019-10-01 18:23:03 +0900

    Add main code

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "./helper/FileHelper.h"
+#include "./model/Graph.h"
+#include "./solver/AbstractSolver.h"
+#include "./solver/BasicGreedy.h"
+
+void runTSP(const char *data_file_name, AbstractSolver *solver){
+  try {
+    auto *fh = new FileHelper();
+    auto nodes = fh->readDataFile(data_file_name);
+    auto *graph = new Graph(nodes);
+
+    solver->setGraph(graph);
+    solver->solveAndMeasureTime();
+
+    cout << "tour: ";
+    for (int node : solver->getTour()) {
+      cout << node << " ";
+    }
+    cout << endl;
+
+    cout << "cost: " << solver->getCost() << endl;
+
+    cout << "time: " << solver->getElapsedTime() << endl;
+  } catch (...) {
+    cout << "ERROR OCCURRED" << endl;
+  }
+}
+
+int main() {
+  runTSP("xqf131.txt", new BasicGreedy());
+
+  return 0;
+}
