cdde983594d26eca6b31176ca1c6d613c46cc384
H.Chihoon <to@ihooni.com>
2019-10-01 18:22:20 +0900

    Create FileHelper class

---
+++ src/helper/FileHelper.h
@@ -0,0 +1,31 @@
+#ifndef TSP_FILEHELPER_H
+#define TSP_FILEHELPER_H
+
+#include <vector>
+#include <utility>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class FileHelper {
+ private:
+  /**
+   * Convert resource data file name to absolute file path.
+   *
+   * @param file_name
+   * @return
+   */
+  const char* convertToAbsolutePath(const char *file_name);
+
+ public:
+  /**
+   * Read the node data set from data file.
+   *
+   * @param file_name
+   * @return
+   */
+  vector<pair<int, int>> readDataFile(const char *file_name);
+};
+
+#endif //TSP_FILEHELPER_H
