868c314c42e04393b3391a9a30bac7cf75d2872a
H.Chihoon <to@ihooni.com>
2019-10-04 01:53:44 +0900

    Not use static const config.
    # Use reference

--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,33 @@
 #include "solver/SequentialGreedy.h"
 #include "./helper/Timer.h"
 
-/************************************/
-/**** Set your test config here. ****/
-/************************************/
-static const char* DATA_FILE = "xpr2308.txt";
-static const int ITERATION = INT_MAX;
-static const double TIME_LIMIT = 58.;
-
-void runTSP(const char *data_file_name, AbstractSolver *solver, int iteration, double time_limit){
+void runTSP(const char *data_file_name, AbstractSolver &solver, int iteration, double time_limit){
   try {
     auto *fh = new FileHelper();
     auto nodes = fh->readDataFile(data_file_name);
     auto *graph = new Graph(nodes);
     auto *timer = new Timer(time_limit);
 
-    solver->setGraph(graph);
+    solver.setGraph(graph);
     timer->start();
-    solver->solve(iteration, *timer);
+    solver.solve(iteration, *timer);
     double total_time = timer->getElapsedTime();
 
     // if tsp solved more than once
-    if (solver->getActualIteration()) {
+    if (solver.getActualIteration()) {
       cout << "tour: ";
-      for (int node : solver->getTour()) {
+      for (int node : solver.getTour()) {
         cout << node << " ";
       }
       cout << endl;
 
-      cout << "cost: " << solver->getCost() << endl;
+      cout << "cost: " << solver.getCost() << endl;
 
       cout << "time: " << total_time << endl;
 
-      cout << "iteration: " << solver->getActualIteration() << endl;
+      cout << "iteration: " << solver.getActualIteration() << endl;
 
-      cout << "avg time: " << total_time / solver->getActualIteration() << endl;
+      cout << "avg time: " << total_time / solver.getActualIteration() << endl;
     } else {
       cout << "Time over" << endl;
     }
@@ -48,7 +41,15 @@ void runTSP(const char *data_file_name, AbstractSolver *solver, int iteration, d
 }
 
 int main() {
-  runTSP(DATA_FILE, new SequentialGreedy(), ITERATION, TIME_LIMIT);
+  /************************************/
+  /**** Set your test config here. ****/
+  /************************************/
+  const char* DATA_FILE = "xpr2308.txt";
+  AbstractSolver *SOLVER = new SequentialGreedy();
+  int ITERATION = INT_MAX;
+  double TIME_LIMIT = 58.;
+
+  runTSP(DATA_FILE, *SOLVER, ITERATION, TIME_LIMIT);
 
   return 0;
 }
